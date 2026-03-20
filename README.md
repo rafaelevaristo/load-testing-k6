04b0ea2306a1f6548eef8b24b56188a06e0c4ebf
rafael.evaristo <rafael.evaristo@gmail.com>
2026-03-20 01:07:56 +0000

    Added CrApi to be tested.

--- README.md
+++ README.md
@@ -5,12 +5,29 @@
 docker compose up -d
 ```
 
+## runing the tests after infra up
+``` bash
+docker compose run --rm k6
+```
+
 ## taking all down
 ``` bash
 docker compose down -d
 ```
 
-## runing the tests after infra up
+# Using CrApi
+
+## run tests
 ``` bash
-docker compose run --rm k6
+docker compose -f docker-compose-crapi-oficial.yml up -d
+```
+
+## run tests
+``` bash
+docker compose -f docker-compose-crapi-oficial.yml run --rm k6
+```
+
+## run tests
+``` bash
+docker compose -f docker-compose-crapi-oficial.yml down
 ```
---
+++ scripts/crapi_test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '5s', target: 20 }, // Sobe para 20 utilizadores
+    { duration: '10s', target: 20 },  // Mantém
+    { duration: '5s', target: 0 },  // Desce
+  ],
+};
+
+export default function () {
+  // 1. Testar a página principal da Oficina (Workshop)
+  let resWorkshop = http.get('http://crapi-workshop:8000/workshop/api/merchant/contact');
+  check(resWorkshop, {
+    'workshop status is 200': (r) => r.status === 200,
+  });
+
+  // 2. Tentar um Login (Endpoint de Identidade)
+  // Nota: Isto vai dar erro 401 porque não temos user criado, 
+  // o que é ótimo para ver erros no Grafana!
+  let loginData = JSON.stringify({ email: 'user@example.com', password: '123' });
+  let params = { headers: { 'Content-Type': 'application/json' } };
+  
+  let resLogin = http.post('http://crapi-identity:8080/identity/api/auth/login', loginData, params);
+  check(resLogin, {
+    'login attempted': (r) => r.status !== 0,
+  });
+
+  sleep(1);
+}
